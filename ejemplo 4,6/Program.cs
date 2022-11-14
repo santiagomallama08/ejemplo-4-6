ff29b9472b2c54f08f27fab071faaca2c7150db1
SANTIAGO <SANTIAGO@LAPTOP-REHH1RB2>
2022-11-13 21:11:57 -0600

    Agregar archivos de proyecto.

---
+++ ejemplo 4,6/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ejercicio
+{
+    public class program
+    {
+        public static void Main()
+        {
+            double cantidad, cantidadaño = 0, ahorromes;
+            for (int i = 1; i < 13; i++)
+            {
+                Console.WriteLine("proceso" + i);
+                Console.WriteLine("ingrese el valor a depositar ");
+                cantidad = double.Parse(Console.ReadLine());
+                cantidadaño = cantidadaño + cantidad;
+                ahorromes = cantidadaño;
+                Console.WriteLine($"el valor de cantidad de mes es de  {ahorromes}");
+            }
+            Console.WriteLine($"el ahorro de un año es de  {cantidadaño} ");
+        }
+    }
+}
